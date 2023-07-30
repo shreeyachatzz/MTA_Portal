836b754e30ed5ce34f62181cc1fbc330fb37b948
Harsh <hjain_be21@thapar.edu>
2023-07-30 17:33:05 +0530

    resources routes and controllers added

--- server/controllers/resource_controllers.js
+++ server/controllers/resource_controllers.js
@@ -3,7 +3,8 @@ import User from '../models/User.js';
 
 export const addResource = async (req, res) => {
   try {
-    const { subject, link } = req.body;
+    const { subject } = req.params;
+    const {link} = req.body;
     const { group } = req.rootUser;
 
     // Create a new resource
@@ -11,8 +12,6 @@ export const addResource = async (req, res) => {
       subject,
       link,
       group, // Set the group value to the user's group
-      subgroup, // Set the subgroup value to the user's subgroup
-      user: req.rootUser._id, // Associate the resource with the user
     });
 
     // Save the resource to the database
@@ -27,3 +26,46 @@ export const addResource = async (req, res) => {
     res.status(500).json({ error: 'An error occurred while adding the resource' });
   }
 };
+
+
+export const viewResourcesBySubject = async (req, res) => {
+  try {
+    const { subject } = req.params;
+    const { group } = req.rootUser;
+
+    const resources = await Resource.find({ subject, group });
+
+    res.status(200).json({ resources });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred while fetching resources' });
+  }
+};
+
+
+export const deleteResource = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const resource = await Resource.findById(id);
+
+    if (!resource) {
+      return res.status(404).json({ error: 'Resource not found' });
+    }
+
+    if (resource.group !== req.rootUser.group) {
+      return res.status(403).json({ error: 'You are not authorized to delete this resource' });
+    }
+
+    await Resource.findByIdAndRemove(id);
+
+    await User.findByIdAndUpdate(req.rootUser._id, { $pull: { resourcesGrp: id } });
+
+    res.status(200).json({ message: 'Resource deleted successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred while deleting the resource' });
+  }
+};
+
+
--- server/routes/resources_routes.js
+++ server/routes/resources_routes.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import { Authenticate } from '../middlewares/auth.js';
+import { addResource, deleteResource, viewResourcesBySubject } from '../controllers/resource_controllers.js';
 
 const resourceRouter = express.Router();
 
+resourceRouter.post('/addresource/:subject',Authenticate,addResource);
+resourceRouter.delete('/delResource/:id',Authenticate,deleteResource);
+resourceRouter.get('/viewResource/:subject',Authenticate,viewResourcesBySubject);
 
 export default resourceRouter;
