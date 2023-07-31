45f987e79f2121dd7d22a96d26b96fc2c4be87b5
SHREEYA CHATTERJI <schatterji_be21@thapar.edu>
2023-07-31 15:04:37 +0530

    sorted imp dates

--- client/src/Pages/User/ImpDates/ICard/ICard.jsx
+++ client/src/Pages/User/ImpDates/ICard/ICard.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import './ICard.css'
 
-const ICard = () => {
+const ICard = ({ title, content, date }) => {
   const isAdmin=true;
   return (
     <div className='card-i'>
     <div className='header'>
     <div className='date-i'>
-            15/06/2023
+            {date}
         </div>
         {isAdmin&& <div className='del-dead'>
               Delete
             </div>}
     </div>
         <div className='info-i'>
-        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ng Lorem Ipsum is that it.
+        {content}
         </div>
     </div>
   )
--- client/src/Pages/User/ImpDates/ImpDates.jsx
+++ client/src/Pages/User/ImpDates/ImpDates.jsx
@@ -5,16 +5,25 @@ import ICard from './ICard/ICard';
 
 const ImpDates = (props, state) => {
   const dataArray = [
-    { title: 'Card 1', content: 'Content 1' },
-    { title: 'Card 2', content: 'Content 2' },
-    { title: 'Card 3', content: 'Content 3' },
-    { title: 'Card 1', content: 'Content 1' },
-    { title: 'Card 2', content: 'Content 2' },
-    { title: 'Card 1', content: 'Content 1' },
-    { title: 'Card 2', content: 'Content 2' },
+    { title: 'Card 1', content: 'Content 1', date: '15/09/2003' },
+    { title: 'Card 2', content: 'Content 2', date: '15/06/2013' },
+    { title: 'Card 3', content: 'Content 3', date: '15/06/2023' },
+    { title: 'Card 1', content: 'Content 1', date: '15/07/2003' },
+    { title: 'Card 2', content: 'Content 2', date: '15/08/2003' },
+    { title: 'Card 3', content: 'Content 3', date: '15/09/2003' },
+    { title: 'Card 1', content: 'Content 1', date: '15/10/2003' },
+    { title: 'Card 2', content: 'Content 2',  date: '15/11/2003' },
+    { title: 'Card 3', content: 'Content 3', date: '15/12/2003' },
+    // Add more objects as needed
   ];
 
   const shouldSetHeight = dataArray.length < 10;
+
+  const sortedDataArray = [...dataArray].sort((a, b) => {
+    const [dayA, monthA, yearA] = a.date.split('/');
+    const [dayB, monthB, yearB] = b.date.split('/');
+    return Date.UTC(yearA, monthA - 1, dayA) - Date.UTC(yearB, monthB - 1, dayB);
+  });
 
   return (
      <div className={`fullmain ${shouldSetHeight ? 'fill' : ''}`}>
@@ -25,8 +34,14 @@ const ImpDates = (props, state) => {
           IMPORTANT DATES
         </div>
         <div className='cards'>
-          {dataArray.map((item, index) => (
-              <ICard key={index} title={item.title}/>
+          {sortedDataArray.map((item, index) => (
+              <ICard 
+                key={index}
+                title={item.title}
+                content={item.content}
+                subject={item.subject}
+                date={item.date}
+              />
             ))}
         </div>
       </div>
