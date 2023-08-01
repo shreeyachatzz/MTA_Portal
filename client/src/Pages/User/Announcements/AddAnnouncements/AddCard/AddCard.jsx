c0386895937ceab0ec9da23ee6a1bb041312d55b
Harsh <hjain_be21@thapar.edu>
2023-08-01 19:46:41 +0530

    integrated backend for ann section

--- client/src/Pages/User/Announcements/AddAnnouncements/AddCard/AddCard.jsx
+++ client/src/Pages/User/Announcements/AddAnnouncements/AddCard/AddCard.jsx
@@ -1,40 +1,102 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AddCard.css'
 import { useNavigate } from 'react-router-dom'
+import { useEditContext } from '../../../../../EditContext'
 
 const AddCard = () => {
-    const navigate = useNavigate()
-    const handleAnnSubmit=(e)=>{
-        e.preventDefault();
-        navigate('/announcements')
-    }
-  return (
-    <div className='full'>
-        <div className='title-a'>
-            Class
-        </div>
-        <div className='context'>
-            Select your Section/Group
-        </div>
-        <div className='select-but'>
-            <button className='sec-grp'>
-                CO16
-            </button>
-            <button className='sec-grp'>
-                CO15-22
-            </button>            
-        </div>
-        <div className='title-a'>
-            Announcement
-        </div>
-        <textarea className="an-detail" placeholder="Write an announcement here...">
-        </textarea>
-        <div className='submiting'>
-        <p className='sub' onClick={handleAnnSubmit}>Submit</p>
-        </div>
+    const { userData, setUserData } = useEditContext();
+    const [clickedButton, setClickedButton] = useState('subgroup');
+    const [description, setDescription] = useState('');
+    const navigate = useNavigate();
 
-    </div>
-  )
+    const token = localStorage.getItem("jwtoken");
+    const handleAnnSubmit = async(e) => {
+        try {
+
+            if (!description) {
+                window.alert('Please fill in all the details.');
+                return;
+              }
+            const requestData = {
+                description,
+            };
+
+            // Determine the appropriate backend route based on the clickedButton value
+            const backendRoute = clickedButton === 'group' ? 'http://localhost:5000/announcement/postMyGrpAnnouncements' : 'http://localhost:5000/announcement/postMySubGrpAnnouncements';
+
+            const response = await fetch(backendRoute, {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify(requestData),
+            });
+
+            const data = await response.json();
+
+            if (response.status === 422) {
+                window.alert('Empty fields! Please fill in all the details.');
+            } else if (response.status === 201) {
+                // Handle success scenario, e.g., show success message, clear form, etc.
+                console.log('Announcement submission successful!');
+                console.log(data);
+
+                // Clear the form fields after successful submission
+                setDescription('');
+
+                // Navigate to another page after successful submission, if required
+                navigate('/announcements'); // Change '/success' to the desired route
+            } else {
+                console.log('Announcement submission failed!');
+                console.log(data);
+            }
+        } catch (error) {
+            // Handle any error that may occur during the API call
+            console.error('Error submitting deadline:', error);
+        }
+    }
+
+    const handleButtonClick = (buttonType) => {
+        setClickedButton(buttonType);
+    };
+
+    const handleDescriptionChange = (event) => {
+        setDescription(event.target.value);
+    };
+
+    return (
+        <div className='full'>
+            <div className='title-a'>
+                Class
+            </div>
+            <div className='context'>
+                Select your Section/Group
+            </div>
+            <button
+                className={`sec-grp ${clickedButton === 'subgroup' ? 'active' : ''}`}
+                onClick={() => handleButtonClick('subgroup')}
+            >
+                {userData.subgroup}
+            </button>
+            <button
+                className={`sec-grp ${clickedButton === 'group' ? 'active' : ''}`}
+                onClick={() => handleButtonClick('group')}
+            >
+                {userData.group}
+            </button>
+            <div className='title-a'>
+                Announcement
+            </div>
+            <textarea className="an-detail" placeholder="Write an announcement here..." onChange={handleDescriptionChange}>
+            </textarea>
+            <div className='submiting'>
+                <p className='sub' onClick={handleAnnSubmit}>Submit</p>
+            </div>
+
+        </div>
+    )
 }
 
 export default AddCard
--- server/controllers/announcement_controllers.js
+++ server/controllers/announcement_controllers.js
@@ -1,55 +1,59 @@
 import Announcement from '../models/Announcement.js';
 import User from '../models/User.js';
+import moment from 'moment';
 
 export const postGroupAnnouncement = async (req, res) => {
-    try {
-        const { title, description } = req.body;
-        const { group } = req.rootUser;
+  try {
+    const { description } = req.body;
+    const { group } = req.rootUser;
 
-        if(!title || !description){
-            res.status(422).json({error : "empty fields"});
-        }
-        const announcement = new Announcement({
-            title,
-            description,
-            group, 
-        });
-
-        await announcement.save();
-
-        await User.findByIdAndUpdate(req.rootUser._id, { $push: { announcementsGrp: announcement._id } });
-
-        res.status(201).json({ message: 'Announcement posted in the group successfully!', announcement });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'An error occurred while posting the announcement.' });
+    const date = moment().format('DD/MM/YYYY hh:mm A');
+    if (!description) {
+      res.status(422).json({ error: "empty fields" });
     }
+    const announcement = new Announcement({
+      date,
+      description,
+      group,
+    });
+
+    await announcement.save();
+
+    await User.findByIdAndUpdate(req.rootUser._id, { $push: { announcementsGrp: announcement._id } });
+
+    res.status(201).json({ message: 'Announcement posted in the group successfully!', announcement });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred while posting the announcement.' });
+  }
 };
 
 export const postSubgroupAnnouncement = async (req, res) => {
-    try {
-        const { title, description } = req.body;
-        const { subgroup } = req.rootUser;
+  try {
+    const { description } = req.body;
+    const { subgroup } = req.rootUser;
 
-        if(!title || !description){
-            res.status(422).json({error : "empty fields"});
-        }
-
-        const announcement = new Announcement({
-            title,
-            description,
-            subgroup, 
-        });
-
-        await announcement.save();
-
-        await User.findByIdAndUpdate(req.rootUser._id, { $push: { announcementsSubGrp: announcement._id } });
-
-        res.status(201).json({ message: 'Announcement posted in the subgroup successfully!', announcement });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'An error occurred while posting the announcement.' });
+    const date = moment().format('DD/MM/YYYY hh:mm A');
+    if (!description) {
+      res.status(422).json({ error: "empty fields" });
     }
+
+
+    const announcement = new Announcement({
+      date,
+      description,
+      subgroup,
+    });
+
+    await announcement.save();
+
+    await User.findByIdAndUpdate(req.rootUser._id, { $push: { announcementsSubGrp: announcement._id } });
+
+    res.status(201).json({ message: 'Announcement posted in the subgroup successfully!', announcement });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred while posting the announcement.' });
+  }
 };
 
 
@@ -78,51 +82,65 @@ export const viewSubgroupAnnouncements = async (req, res) => {
 
 
 export const deleteGroupAnnouncement = async (req, res) => {
-    try {
-      const { id } = req.params;
-  
-      const announcement = await Announcement.findById(id);
-      if (!announcement) {
-        return res.status(404).json({ error: 'Announcement not found' });
-      }
+  try {
+    const { id } = req.params;
 
-      if (announcement.group !== req.rootUser.group) {
-        return res.status(403).json({ error: 'You are not authorized to delete this announcement' });
-      }
-  
-      await Announcement.findByIdAndRemove(id);
-  
-      await User.findByIdAndUpdate(req.rootUser._id, { $pull: { announcementsGrp: id } });
-  
-      res.status(200).json({ message: 'Group announcement deleted successfully' });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'An error occurred while deleting the announcement' });
+    const announcement = await Announcement.findById(id);
+    if (!announcement) {
+      return res.status(404).json({ error: 'Announcement not found' });
     }
-  };
 
-
-
-  export const deleteSubgroupAnnouncement = async (req, res) => {
-    try {
-      const { id } = req.params;
-
-      const announcement = await Announcement.findById(id);
-      if (!announcement) {
-        return res.status(404).json({ error: 'Announcement not found' });
-      }
-  
-      if (announcement.subgroup !== req.rootUser.subgroup) {
-        return res.status(403).json({ error: 'You are not authorized to delete this announcement' });
-      }
-  
-      await Announcement.findByIdAndRemove(id);
-
-      await User.findByIdAndUpdate(req.rootUser._id, { $pull: { announcementsSubGrp: id } });
-  
-      res.status(200).json({ message: 'Subgroup announcement deleted successfully' });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'An error occurred while deleting the announcement' });
+    if (announcement.group !== req.rootUser.group) {
+      return res.status(403).json({ error: 'You are not authorized to delete this announcement' });
     }
-  };
+
+    await Announcement.findByIdAndRemove(id);
+
+    await User.findByIdAndUpdate(req.rootUser._id, { $pull: { announcementsGrp: id } });
+
+    res.status(200).json({ message: 'Group announcement deleted successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred while deleting the announcement' });
+  }
+};
+
+
+
+export const deleteSubgroupAnnouncement = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const announcement = await Announcement.findById(id);
+    if (!announcement) {
+      return res.status(404).json({ error: 'Announcement not found' });
+    }
+
+    await Announcement.findByIdAndRemove(id);
+
+    await User.findByIdAndUpdate(req.rootUser._id, { $pull: { announcementsSubGrp: id } });
+
+    res.status(200).json({ message: 'Subgroup announcement deleted successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred while deleting the announcement' });
+  }
+};
+
+export const viewAllAnnouncements = async (req, res) => {
+  try {
+    const { subgroup, group } = req.rootUser;
+    const subgroupString = subgroup.toString();
+
+    // Find announcements for the subgroup and group of the user
+    const subgroupAnnouncements = await Announcement.find({ subgroup : subgroupString });
+    const groupAnnouncements = await Announcement.find({ group });
+
+    const allAnnouncements = subgroupAnnouncements.concat(groupAnnouncements);
+
+    res.status(200).json({announcements : allAnnouncements});
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An error occurred while fetching announcements' });
+  }
+};
