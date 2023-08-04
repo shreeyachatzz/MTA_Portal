11afc7eff5609fbe1b92e1850df29723608bea08
Harsh <hjain_be21@thapar.edu>
2023-08-04 15:36:01 +0530

    resources section integration complete

--- client/src/Pages/User/MainPage/AddStudy/StudyAdd/StudyAdd.jsx
+++ client/src/Pages/User/MainPage/AddStudy/StudyAdd/StudyAdd.jsx
@@ -1,41 +1,81 @@
-import React from 'react'
-import {Link, Routes, Route, useNavigate} from 'react-router-dom';
-
-import './StudyAdd.css'
-import DropdownAdd from '../../../../../Components/DropDownPop/DropDownAdd';
+import React, { useState } from 'react';
+import { Link, Routes, Route, useNavigate } from 'react-router-dom';
+import './StudyAdd.css';
 
 const StudyAddCard = () => {
-
   const navigate = useNavigate();
+  const [addBtn,setAddBtN] = useState('Add');
+  const [inputData, setInputData] = useState({ subject: '', link: '' });
 
-  const handleClickSub = event => {
+  const handleClickSub = async (event) => {
+    setAddBtN('Adding ...');
     event.preventDefault();
 
-    // 👇️ redirect to /contacts
-    navigate('/study');
+    try {
+      const response = await fetch('http://localhost:5000/resource/addResource', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('jwtoken')}`,
+        },
+        body: JSON.stringify(inputData),
+      });
+
+      const data = response.json();
+
+      if (response.status === 400) {
+        setAddBtN('Add');
+        window.alert('Empty fields!');
+      }
+
+      else{
+        setAddBtN('Add');
+        navigate('/study');
+      }
+    } catch (error) {
+      navigate('/login');
+      console.error(error);
+      // Handle error if needed
+    }
   };
-    const dropdownItems = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className='full'>
-    <div className='title-a'>
-        Subject
-    </div>
-    <div className='justlikethat'>
- 
-    <DropdownAdd items={dropdownItems}/>
-    </div>
-    <div className='title-a'>
+      <div className='title-a'>Subject</div>
+      <input
+        className='an-details-sm'
+        placeholder='Enter title'
+        type='text'
+        name='subject'
+        onChange={handleChange}
+      />
+      <div className='title-a'>
+        <br />
         Link
+      </div>
+      <input
+        className='an-details-sm'
+        placeholder='Paste link here...'
+        type='text'
+        name='link'
+        onChange={handleChange}
+      />
+      <div className='break'></div>
+      <div className='submiting'>
+        <p className='sub' onClick={handleClickSub}>
+          {addBtn}
+        </p>
+      </div>
     </div>
-    
-    <input className="an-details-sm" placeholder="Paste link here..." type='link'>
-    </input>
-    <div className='break'></div>
-    <div className='submiting'>
-        <p className='sub' onClick={handleClickSub}>Submit</p>
-        </div>
-  </div>
-  )
-}
+  );
+};
 
-export default StudyAddCard
+export default StudyAddCard;
