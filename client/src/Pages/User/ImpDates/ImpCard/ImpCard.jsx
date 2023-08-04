701b906a0ee532f53c0175835b9daa5b0106ae65
SHREEYA CHATTERJI <schatterji_be21@thapar.edu>
2023-08-04 09:36:07 +0530

    imp dates filter added

--- client/src/Pages/User/ImpDates/ImpCard/ImpCard.jsx
+++ client/src/Pages/User/ImpDates/ImpCard/ImpCard.jsx
@@ -5,24 +5,28 @@ import { MdOutlineDeleteForever } from 'react-icons/md';
 const ImpCard = ({subject,date,time,venue,type}) => {
     const isAdmin=true;
   return (
-    <div className='card-imp-d'>
-    
-    <div className='card-imp'>
-    <div className='sub-box'>
-    <div className='subj'>{subject}</div>
-    </div>
-      <div className='dtogether'>
-        <div className='date'>{date}</div>
-        <div className='time'>{time}</div>
+    <>
+      <div>
+      {isAdmin&&<div className='card-imp-d'>
+      
+      <div className='card-imp'>
+      <div className='sub-box'>
+      <div className='subj'>{subject}</div>
       </div>
-      <div className='tven'>
-        <div className='type'>{type}</div>
-        <div className='venmob'>{venue}</div>
+        <div className='dtogether'>
+          <div className='date'>{date}</div>
+          <div className='time'>{time}</div>
+        </div>
+        <div className='tven'>
+          <div className='type'>{type}</div>
+          <div className='venmob'>{venue}</div>
+        </div>
+        <div className='venue'>{venue}</div>
+        {isAdmin&&<div className='del-impdate'><MdOutlineDeleteForever/></div>}
       </div>
-      <div className='venue'>{venue}</div>
-      {isAdmin&&<div className='del-impdate'><MdOutlineDeleteForever/></div>}
-    </div>
-    </div>
+      </div>}
+      </div>
+    </>
   )
 }
 
--- client/src/Pages/User/ImpDates/ImpDates.jsx
+++ client/src/Pages/User/ImpDates/ImpDates.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import './ImpDates.css'
-import SideNav from '../../../Components/Navbar/Navbar'
-import ICard from './ICard/ICard';
+import React, { useState } from 'react';
+import './ImpDates.css';
+import SideNav from '../../../Components/Navbar/Navbar';
 import ImpCard from './ImpCard/ImpCard';
 
-const ImpDates = (props, state) => {
+const ImpDates = (props) => {
+  const [selectedButton, setSelectedButton] = useState('');
+
   const dataArray = [
-    { subject: 'Network Programming', date: '15/06/03', time: '12.30pm', venue:'LT101',type:'SESS'},
-    { subject: 'Software Engineering', date: '15/06/13', time: '12.30pm', venue:'-' , type:'SESS'},
-    { subject: 'Machine Learning', date: '15/06/23', time: '12.30pm', venue:'LT101' ,type:'SESS'},
-    { subject: 'ML Lab', date: '15/06/33', time: '12.30pm', venue:'LT101',type:'EST' },
-    { subject: 'SE Lab', date: '15/06/43', time: '12.30pm', venue:'LT101', type:'MST' },
-    { subject: 'EDS', date: '15/06/53', time: '12.30pm', venue:'LT101' ,type:'EST'}, 
-    { subject: 'EDS', date: '15/06/53', time: '12.30pm', venue:'LT101' ,type:'EST'},    
+    { subject: 'Network Programming', date: '15/06/03', time: '12.30pm', venue:'LT101',type:'SESS', class: 'co16'},
+    { subject: 'Software Engineering', date: '15/06/13', time: '12.30pm', venue:'-' , type:'SESS',class: 'co16-20'},
+    { subject: 'Machine Learning', date: '15/06/23', time: '12.30pm', venue:'LT101' ,type:'SESS', class: 'co16'},
+    { subject: 'ML Lab', date: '15/06/33', time: '12.30pm', venue:'LT101',type:'EST', class: 'co16-20' },
+    { subject: 'SE Lab', date: '15/06/43', time: '12.30pm', venue:'LT101', type:'MST', class: 'co16-20' },
+    { subject: 'EDS', date: '15/06/53', time: '12.30pm', venue:'LT101' ,type:'EST',class: 'co16'}, 
+    { subject: 'EDS', date: '15/06/53', time: '12.30pm', venue:'LT101' ,type:'EST', class: 'co16-20'},    
     // Add more objects as needed
   ];
-
+  
   const shouldSetHeight = dataArray.length < 10;
 
   const sortedDataArray = [...dataArray].sort((a, b) => {
@@ -24,40 +25,48 @@ const ImpDates = (props, state) => {
     return Date.UTC(yearA, monthA - 1, dayA) - Date.UTC(yearB, monthB - 1, dayB);
   });
 
+  const handlegrp = () => {
+    setSelectedButton((prevSelected) => (prevSelected === 'co16' ? '' : 'co16'));
+  };
+
+  const filteredDataArray = sortedDataArray.filter((item) => {
+    return selectedButton === '' || item.class === selectedButton;
+  });
+
   return (
-     <div className={`fullmain ${shouldSetHeight ? 'fill' : ''}`}>
-      <SideNav heading='IMPORTANT DATES'/>
-      
+    <div className={`fullmain ${shouldSetHeight ? 'fill' : ''}`}>
+      <SideNav heading='IMPORTANT DATES' />
 
       <div className='containerr-d'>
-        <div className='mobHead'>
-          IMPORTANT DATES
+        <div className='mobHead'>IMPORTANT DATES</div>
+        <div className={`butf harshlove ${selectedButton === 'co16' ? 'active' : ''}`} onClick={handlegrp}>
+          CO16
         </div>
         <div className='cards'>
-        <div className='card-imp1'>
+          <div className='card-imp1'>
             <div className='subj1'>SUBJECT</div>
-            
             <div className='dtogether1'>
               <div className='date1'>DATE/TIME</div>
             </div>
-            <div className='type1'>TYPE<p className='type-space'>/VENUE</p></div>
+            <div className='type1'>
+              TYPE<p className='type-space'>/VENUE</p>
+            </div>
             <div className='venue1'>VENUE</div>
-        </div>
-          {sortedDataArray.map((item, index) => (
-              <ImpCard
-                key={index}
-                subject={item.subject}
-                date={item.date}
-                time={item.time}
-                venue={item.venue}
-                type={item.type}
-              />
-            ))}
+          </div>
+          {filteredDataArray.map((item, index) => (
+            <ImpCard
+              key={index}
+              subject={item.subject}
+              date={item.date}
+              time={item.time}
+              venue={item.venue}
+              type={item.type}
+            />
+          ))}
         </div>
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default ImpDates
+export default ImpDates;
