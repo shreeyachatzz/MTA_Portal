c0ad483c030a94e371b7374e1233ad60aebedc53
Harsh <hjain_be21@thapar.edu>
2023-08-07 19:46:54 +0530

    integrated backend for add resources

--- client/src/Pages/User/MainPage/AddStudy/StudyAdd/StudyAdd.jsx
+++ client/src/Pages/User/MainPage/AddStudy/StudyAdd/StudyAdd.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './StudyAdd.css';
+import { useEditContext } from '../../../../../EditContext';
 
 const StudyAddCard = () => {
+  const { userData, setUserData } = useEditContext();
   const navigate = useNavigate();
   const [addBtn, setAddBtn] = useState('Add');
   const [inputData, setInputData] = useState({ subject: '', link: '' });
@@ -12,22 +14,24 @@ const StudyAddCard = () => {
     setSelectedSection(section);
   };
 
-  const handleClickSub = async (event) => {
+  const handleResourceAdd = async (event) => {
     event.preventDefault();
     setAddBtn('Adding ...');
 
     try {
-      const response = await fetch('http://localhost:5000/resource/addResource', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${localStorage.getItem('jwtoken')}`,
-        },
-        body: JSON.stringify({
-          ...inputData,
-          section: selectedSection,
-        }),
-      });
+      const response = await fetch(
+        selectedSection === 'subgroup'
+          ? 'http://localhost:5000/resource/addSubGrpResource'
+          : 'http://localhost:5000/resource/addGrpResource',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('jwtoken')}`,
+          },
+          body: JSON.stringify(inputData),
+        }
+      );
 
       if (response.status === 400) {
         setAddBtn('Add');
@@ -53,7 +57,7 @@ const StudyAddCard = () => {
 
   return (
     <div className='full'>
-      <form onSubmit={handleClickSub}>
+      <form onSubmit={handleResourceAdd}>
         <div className='title-a'>Subject</div>
         <input
           className='an-details-sm'
@@ -62,29 +66,30 @@ const StudyAddCard = () => {
           name='subject'
           onChange={handleChange}
         />
-        <div className='title-a sm-cl'>
-          Class
-        </div>
-        <div className='context'>
-          Select your Section/Group
-        </div>
+        <div className='title-a sm-cl'>Class</div>
+        <div className='context'>Select your Section/Group</div>
         <div className='sm-but-cl'>
-        <div
-          className={`sec-grp ${selectedSection === 'subgroup' ? 'active' : ''}`}
-          onClick={() => handleButtonClick('subgroup')}
-        >
-          Subgroup
+          <button
+            className={`sec-grp ${selectedSection === 'subgroup' ? 'active' : ''}`}
+            onClick={(e) => {
+              e.preventDefault(); // Prevent form submission
+              handleButtonClick('subgroup');
+            }}
+          >
+            {userData.subgroup}
+          </button>
+          <button
+            className={`sec-grp ${selectedSection === 'group' ? 'active' : ''}`}
+            onClick={(e) => {
+              e.preventDefault(); // Prevent form submission
+              handleButtonClick('group');
+            }}
+          >
+            {userData.group}
+          </button>
         </div>
-        <div
-          className={`sec-grp ${selectedSection === 'group' ? 'active' : ''}`}
-          onClick={() => handleButtonClick('group')}
-        >
-          Group
-        </div>
-        </div>
-        <div className='title-a'>
-          Link
-        </div>
+
+        <div className='title-a'>Link</div>
         <input
           className='an-details-sm'
           placeholder='Paste link here...'
