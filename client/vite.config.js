a759f3807bd3c9d73ae4594d496977d7ae77ad47
Harsh <hjain_be21@thapar.edu>
2023-08-14 18:47:07 +0530

    changed vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://mtaportal.vercel.app/',
+  base:'/',
   plugins: [react()],
 })
