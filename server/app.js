74b45e74bfe87917a5059b6da2d23625d4e0a96e
SHREEYA CHATTERJI <schatterji_be21@thapar.edu>
2023-08-24 01:54:14 +0530

    changed cors origin

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ connectDB();
 //middlewares
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://mtaportal.netlify.app/',
+    origin: 'https://mtaportal.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
